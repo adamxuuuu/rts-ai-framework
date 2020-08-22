23209265971fc13bbddd3114c679c35c4e8f2c2b
adamxu <1254420756xyd@gmail.com>
2020-08-22 11:57:08 +0100

    Action reformat

---
+++ src/core/action/Train.java
@@ -0,0 +1,66 @@
+package core.action;
+
+import core.Constants;
+import core.entity.Building;
+import core.entity.Unit;
+import core.game.GameState;
+
+public class Train extends Action {
+
+    // TODO should not pass a <Unit> to the constructor
+    private final Unit unit;
+
+    private final int playerId;
+    private long buildTime;
+    private int costRemain;
+
+    public Train(Unit unit) {
+        this.unit = unit;
+
+        playerId = unit.getAgentId();
+        buildTime = unit.getBuildTime() * Constants.SECOND_NANO;
+        costRemain = unit.getCost();
+    }
+
+    private Train(Unit unit, int playerId, long buildTime, int costRemain) {
+        this.unit = unit;
+        this.playerId = playerId;
+        this.buildTime = buildTime;
+        this.costRemain = costRemain;
+    }
+
+    @Override
+    public void exec(GameState gs, double elapsed) {
+        // Check resource first time execute
+        if (!gs.checkResource(playerId, unit.getCost()) && costRemain != 0) {
+            isComplete = true;
+            return;
+        } else {
+            // Handle cost reduction
+            gs.handleResource(playerId, -costRemain);
+            costRemain = 0;
+        }
+
+
+        buildTime -= elapsed;
+        if (buildTime > 0) {
+            // Building in progress
+            return;
+        }
+
+        Building source = gs.getGrid().getBuilding(unit.getAgentId(), Building.Type.BASE);
+        gs.addUnit(unit, source.getGridPos());
+
+        isComplete = true;
+    }
+
+    @Override
+    public Action copy() {
+        return new Train(unit, playerId, buildTime, costRemain);
+    }
+
+    @Override
+    public long actorId() {
+        return 0;
+    }
+}
